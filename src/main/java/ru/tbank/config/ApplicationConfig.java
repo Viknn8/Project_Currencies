c011463546a8ee5d765511a02174c175ab95ddfe
Viknn8 <viktory.na.2610@gmail.com>
2025-04-24 23:22:53 +0300

    джоба в неосновную ветку

---
+++ src/main/java/ru/tbank/config/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package ru.tbank.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
